11c6f2d592c9902fb704f2ba80e79c906ba15f2f
HiLMadara <71180913+HiLMadara@users.noreply.github.com>
2021-02-24 22:31:25 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,29 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "generate.h"
 
 
-#define TEST_LIMIT 100001
 
-int
-main(int argc, char** argv)
+int main(int argc, char ** argv)
 {
+    int test = atoi(argv[1]);
     int a[10];
     double sum = 0;
-
+    int sch=0;
     srand(time(NULL));
-    memset(a, 0, sizeof(int) * 10);
-
-    for (int i = 0; i < TEST_LIMIT; i++) {
-        a[rand() % 10]++;
+    for (int i = 0; i < 10; i++) 
+     {
+        a[i] = 0;
+     }
+    for (int i = 0; i < test; i++) 
+    {
+        sch =  generate();
+        a[sch]++;
     }
+   for (int i = 0; i < 10; i++) {
+       double p = a[i] / (double)100000;
+       sum += p;
+       printf( "chance of %i = %lf\n", i, p);
+   }
+   printf( "sum of chances are: %.2lf\n", sum);
 
-    for (int i = 0; i < 10; i++) {
-        double p = a[i] / (double)100000;
-        sum += p;
-        fprintf(stdout, "chance of %i = %lf\n", i, p);
-    }
-    fprintf(stdout, "sum of chances are: %.2lf\n", sum);
-
-    exit(EXIT_SUCCESS);
+   exit(EXIT_SUCCESS);
 }
